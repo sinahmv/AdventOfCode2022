35998eba1886b9dc66e256c0f196044c7f85986a
sinah <sinah.mv@gmail.com>
2022-12-19 15:06:29 +0100

    Day 5 working

--- Day5/main.go
+++ Day5/main.go
@@ -5,41 +5,56 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"strconv"
 )
 
-type stack struct {
+type ship struct {
 	id  int
-	content []string
+	resources []string
 }
 
-func moveItems() {
+func moveItems(ship []ship, amount int, startship int, endship int) []ship {
 
+}
+
+func getResult(currentShip []ship) []string {
+	result := make([]string, 0)
+	upperResource := ""
+	for i := 0; i < len(currentShip); i++ {
+		upperResource = currentShip[i].resources[len(currentShip[i].resources)-1]
+		result = append(result, upperResource)
+	}
+	return result
 }
 
 func main() {
 
-	/*var ship []stack = []stack{
-		{id: 1, content: []string{"T", "R", "G", "W", "Q", "M", "F", "P"}},
-		{id: 2, content: []string{"R", "F", "H"}},
-		{id: 3, content: []string{"D", "S", "H", "G", "V", "R", "Z", "P"}},
-		{id: 4, content: []string{"G", "W", "F", "B", "P", "H", "Q"}},
-		{id: 5, content: []string{"H", "J", "M", "S", "P"}},
-		{id: 6, content: []string{"L", "P", "R", "S", "H", "T", "Z", "M"}},
-		{id: 7, content: []string{"L", "M", "N", "H", "T", "P"}},
-		{id: 8, content: []string{"R", "Q", "D", "F"}},
-		{id: 9, content: []string{"H", "P", "L", "N", "C", "S", "D"}},
-	}*/
-
 	f, _ := os.Open("/Users/sinah/Code/AdventOfCode2022/Day5/input.txt")
 	scanner := bufio.NewScanner(f)
-	result := make([]string, 0)
+	var currentShip []ship = []ship{
+		{id: 1, resources: []string{"T", "R", "G", "W", "Q", "M", "F", "P"}},
+		{id: 2, resources: []string{"R", "F", "H"}},
+		{id: 3, resources: []string{"D", "S", "H", "G", "V", "R", "Z", "P"}},
+		{id: 4, resources: []string{"G", "W", "F", "B", "P", "H", "Q"}},
+		{id: 5, resources: []string{"H", "J", "M", "S", "P"}},
+		{id: 6, resources: []string{"L", "P", "R", "S", "H", "T", "Z", "M"}},
+		{id: 7, resources: []string{"L", "M", "N", "H", "T", "P"}},
+		{id: 8, resources: []string{"R", "Q", "D", "F"}},
+		{id: 9, resources: []string{"H", "P", "L", "N", "C", "S", "D"}},
+	}
 
 	for scanner.Scan() {
 
 		input := strings.Split(scanner.Text(), "/n")
-		
-		
+
+		for i := 0; i < len(input); i++ {
+			rearrangements := strings.Split(input[i], " ")
+			amount, _ := strconv.Atoi(rearrangements[1])
+			startship, _ := strconv.Atoi(rearrangements[3])
+			endship, _ := strconv.Atoi(rearrangements[5])
+			moveItems(currentShip, amount, startship, endship)
+		}
 
 	}
-	fmt.Print(result)
+	fmt.Print(getResult(currentShip))
 }
