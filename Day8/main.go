cad1159adb6743e6674be53ffc47f77d75a15fb0
sinah <sinah.mv@gmail.com>
2023-01-14 18:18:34 +0100

    Day 8 Part 2 finally by me

--- Day8/main.go
+++ Day8/main.go
@@ -19,21 +19,16 @@ func main() {
 
 func calculateVisibleTrees(trees []string) int {
 
-	//??????????
 	visibleTrees := map[string]struct{}{}
-	visibleTrees2 := 0
 
 	for row := 1; row < len(trees)-1; row++ {
 
-		visibleTrees2++
 		// Links nach Rechts
 		tallestTree, _ := strconv.Atoi(string(trees[row][0]))
 		for col := 1; col < len(trees[row])-1; col++ {
 			currentTree, _ := strconv.Atoi(string(trees[row][col]))
 			if currentTree > tallestTree {
 				tallestTree = currentTree
-				visibleTrees2++
-				//??????????
 				visibleTrees[strconv.Itoa(row)+" "+strconv.Itoa(col)] = struct{}{}
 			}
 		}
@@ -44,8 +39,6 @@ func calculateVisibleTrees(trees []string) int {
 			currentTree, _ := strconv.Atoi(string(trees[row][col]))
 			if currentTree > tallestTree {
 				tallestTree = currentTree
-				visibleTrees2++
-				//????????????
 				visibleTrees[strconv.Itoa(row)+" "+strconv.Itoa(col)] = struct{}{}
 			}
 		}
@@ -53,15 +46,12 @@ func calculateVisibleTrees(trees []string) int {
 
 	for col := 1; col < len(trees)-1; col++ {
 
-		visibleTrees2++
 		// Oben nach Unten
 		tallestTree, _ := strconv.Atoi(string(trees[0][col]))
 		for row := 1; row < len(trees)-1; row++ {
 			currentTree, _ := strconv.Atoi(string(trees[row][col]))
 			if currentTree > tallestTree {
 				tallestTree = currentTree
-				visibleTrees2++
-				//??????????
 				visibleTrees[strconv.Itoa(row)+" "+strconv.Itoa(col)] = struct{}{}
 			}
 		}
@@ -72,43 +62,42 @@ func calculateVisibleTrees(trees []string) int {
 			currentTree, _ := strconv.Atoi(string(trees[row][col]))
 			if currentTree > tallestTree {
 				tallestTree = currentTree
-				visibleTrees2++
-				//???????????
+
 				visibleTrees[strconv.Itoa(row)+" "+strconv.Itoa(col)] = struct{}{}
 			}
 		}
 	}
-
-	visibleTrees2 = visibleTrees2 - 4
-	fmt.Println(visibleTrees2)
 
 	//Einmal alle Bäume die am Rand auf jeden Fall sichtbar sind und die 4 Ecken werden abgezogen
 	return (len(trees) * 2) + (len(trees[0]) * 2) + len(visibleTrees) - 4
 }
 
 func calcBestScenicScore(trees []string) int {
-	bestScore := 0
+
+	highestScore := 0
+
 	for row := 0; row < len(trees)-1; row++ {
 		for col := 0; col < len(trees[row])-1; col++ {
 			currentScore := calcScenicScore(trees, row, col)
-			if currentScore > bestScore {
-				bestScore = currentScore
+			if currentScore > highestScore {
+				highestScore = currentScore
 			}
 		}
 	}
-	return bestScore
+	return highestScore
 }
 
 func calcScenicScore(trees []string, row int, col int) int {
+
 	treeHeight, _ := strconv.Atoi(string(trees[row][col]))
 
-	// check top
+	// oben
 	topScore := 0
 	for r := row - 1; ; r-- {
+		//oberste Reihe
 		if r < 0 {
 			break
 		}
-
 		currentTree, _ := strconv.Atoi(string(trees[r][col]))
 		if currentTree <= treeHeight {
 			topScore++
@@ -119,56 +108,53 @@ func calcScenicScore(trees []string, row int, col int) int {
 		}
 	}
 
-	// check bottom
+	// unten
 	bottomScore := 0
 	for r := row + 1; ; r++ {
+		//unterste Reihe
 		if r == len(trees) {
 			break
 		}
-
 		currentTree, _ := strconv.Atoi(string(trees[r][col]))
 		if currentTree <= treeHeight {
 			bottomScore++
 		}
-
 		if currentTree >= treeHeight {
 			break
 		}
 	}
 
-	// check right
-	rightScore := 0
-	for c := col + 1; ; c++ {
-		if c == len(trees[row]) {
-			break
-		}
-
-		currentTree, _ := strconv.Atoi(string(trees[row][c]))
-		if currentTree <= treeHeight {
-			rightScore++
-		}
-
-		if currentTree >= treeHeight {
-			break
-		}
-	}
-
-	// check left
+	// links
 	leftScore := 0
 	for c := col - 1; ; c-- {
+		//Anfang der Zeile
 		if c < 0 {
 			break
 		}
-
 		currentTree, _ := strconv.Atoi(string(trees[row][c]))
 		if currentTree <= treeHeight {
 			leftScore++
 		}
-
 		if currentTree >= treeHeight {
 			break
 		}
 	}
 
-	return topScore * bottomScore * rightScore * leftScore
+	// rechts
+	rightScore := 0
+	for c := col + 1; ; c++ {
+		//Ende der Zeile
+		if c == len(trees[row]) {
+			break
+		}
+		currentTree, _ := strconv.Atoi(string(trees[row][c]))
+		if currentTree <= treeHeight {
+			rightScore++
+		}
+		if currentTree >= treeHeight {
+			break
+		}
+	}
+	scenicScore := (topScore * bottomScore * rightScore * leftScore)
+	return (scenicScore)
 }
